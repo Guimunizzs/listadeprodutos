ac8411ccb5734e4a63b763d2e01327ff0ef455ef
Guilherme Muniz <gui.munizalemao@gmail.com>
2025-01-04 17:15:14 -0300

    feat: adicionar declaração de variáveis para manipulação do DOM

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const products = [];
+
+  const formContainer = document.getElementById("form-container");
+  const listContainer = document.getElementById("list-container");
+  const productForm = document.getElementById("product-form");
+  const productList = document.getElementById("product-list");
+});
